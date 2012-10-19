979a0476b01211368a378ed15d78e29612de7b0a
Dominique Guinard <dev@guinard.org>
2012-10-19 18:39:06 +0200

    Added new CS modules

--- cs/lib/evrythng/services/init.js
+++ cs/lib/evrythng/services/init.js
@@ -1,9 +1,16 @@
 dojo.provide("cs.lib.evrythng.services.init");
 
 var proxyUrl = "http://localhost:8001";
-var apiKeyGlobal = "KEY";
-var thngId = "50769af8e4b0a4cecca9b83b" // "507e77ace4b0cc3d15e38212"; // Local
-var thngStoreUrl = proxyUrl + "/thngs/" + thngId;
+var apiKeyGlobal = "hep12ZDr4cKXkEeR9Vs1NwYYl1w4vj31tyVSCXbEXVzyJ1Cjng0KLshcJNc88bUaHCTv6iQvgjyNJc4g";
+var coffeeMachineId = "50812fcfe4b0a4cecca9b847" // "507e77ace4b0cc3d15e38212"; // Local
+var coffeeMachineUrl = proxyUrl + "/thngs/" + coffeeMachineId;
+
+var checkinId = "50812e22e4b0a4cecca9b845";
+var checkinUrl = proxyUrl + "/thngs/" + checkinId;
+
+var spotId = "50812b69e4b0a4cecca9b843";
+var spotUrl = proxyUrl + "/thngs/" + spotId;
+
 var evrythngMessage = "EVRYTHNG -- ";
 
 /**
@@ -29,7 +36,7 @@ cs.componentContainer.push({
 	image: "evrythng/evrythng-square.png",
 	exec : function(state){
 		var propertyName = state.inputs.item(0).getValue();
-		var endPointUrl = thngStoreUrl + "/properties/" + propertyName + "?access_token=" + apiKeyGlobal;
+		var endPointUrl = coffeeMachineUrl + "/properties/" + propertyName + "?access_token=" + apiKeyGlobal;
 		this.setAsync();
 		var compo = this;
 		$.ajax({
@@ -86,8 +93,10 @@ cs.componentContainer.push({
 		var compo = this;
 		
 		var propertyName = state.inputs.item(0).getValue();
-		var endPointUrl = thngStoreUrl + "/properties/" + propertyName + "?access_token=" + apiKeyGlobal;
-		var payload = [{"value" : state.inputs.item(1).getValue()}];
+		var endPointUrl = coffeeMachineUrl + "/properties/" + propertyName + "?access_token=" + apiKeyGlobal;
+		var payload = [{
+			"value" : state.inputs.item(1).getValue()
+		}];
 		payload = JSON.stringify(payload);
 
 		$.ajax({
@@ -190,9 +199,10 @@ cs.componentContainer.push({
 		this.setAsync();
 		var compo = this;
 		
-		var propertyName = state.inputs.item(0).getValue();
-		var endPointUrl = thngStoreUrl + "/properties/cmd?access_token=" + apiKeyGlobal;
-		var payload = [{"value" : state.inputs.item(1).getValue()}];
+		var endPointUrl = coffeeMachineUrl + "/properties/cmd?access_token=" + apiKeyGlobal;
+		var payload = [{
+			"value" : state.inputs.item(1).getValue()
+		}];
 		payload = JSON.stringify(payload);
 
 		$.ajax({
@@ -219,3 +229,161 @@ cs.componentContainer.push({
 		});
 	}
 });
+
+/**
+ * This redeems a checkin
+ */
+cs.componentContainer.push({
+	name : "cs.evrythng.demo.redeem",
+	description : "Redeems a checkin.",
+	inputs :
+	[
+	{
+		name: "Name of the property to update",
+		type: "cs.type.String"
+	}
+	],
+	outputs:
+	[
+	{
+		name: "result",
+		type: "cs.type.String"
+	}
+	],
+	image: "evrythng/redeem.png",
+	exec : function(state){
+		this.setAsync();
+		var compo = this;
+		
+		var propertyName = state.inputs.item(0).getValue();
+		var endPointUrl = checkinUrl + "/properties/" + propertyName + "?access_token=" + apiKeyGlobal;
+		var payload = [{
+			"value" : "empty"
+		}];
+		payload = JSON.stringify(payload);
+
+		$.ajax({
+			beforeSend: function(xhrObj){
+				xhrObj.setRequestHeader("Content-Type","application/json");
+				xhrObj.setRequestHeader("Accept","application/json");
+				xhrObj.setRequestHeader("Authorization", apiKeyGlobal);
+			},
+			url: endPointUrl,
+			type: "PUT",
+			data: payload,
+			dataType: "json",
+			success: function(result){
+				state.outputs.item(0).setValue("Points redeemed!");
+				compo.finishAsync();
+			},
+			error: function(msg){
+				compo.finishAsync();
+				state.outputs.item(0).setValue("error");
+				console.log(evrythngMessage + "Error on: " + endPointUrl);
+				console.log(msg);
+				
+			}
+		});
+	}
+});
+
+/**
+ * Get a checkin
+ */
+cs.componentContainer.push({
+	name : "cs.evrythng.demo.checkin",
+	description : "Gets a checkin",
+	inputs :
+	[
+	{
+		name: "Name of the checkin",
+		type: "cs.type.String"
+	}
+	],
+	outputs:
+	[
+	{
+		name: "Name of the person who checked in",
+		type: "cs.type.String"
+	}
+	],
+	image: "evrythng/checkin.png",
+	exec : function(state){
+		var propertyName = state.inputs.item(0).getValue();
+		
+		var endPointUrl = checkinUrl + "/properties/" + propertyName + "?access_token=" + apiKeyGlobal;
+		this.setAsync();
+		var compo = this;
+		$.ajax({
+			beforeSend: function(xhrObj){
+				xhrObj.setRequestHeader("Accept","application/javascript");
+				xhrObj.setRequestHeader("Authorization", apiKeyGlobal);
+			},
+			url: endPointUrl,
+			type: "GET",
+			dataType: "json",
+			success: function(result){
+				// transform result into JSON
+				state.outputs.item(0).setValue(result[0].value);
+				compo.finishAsync();
+			},
+			error: function(msg){
+				compo.finishAsync();
+				console.log(evrythngMessage + "Error on: " + endPointUrl);
+				console.log(msg);
+			}
+		});
+
+	}
+});
+
+/**
+ * Get data from the SunSpot
+ */
+cs.componentContainer.push({
+	name : "cs.evrythng.demo.sunspot",
+	description : "Gets sensor data from an EVRYTHNG-connected SunSpot",
+	inputs :
+	[
+	{
+		name: "Name of the sensor",
+		type: "cs.type.String"
+	}
+	],
+	outputs:
+	[
+	{
+		name: "Value of the sensor",
+		type: "cs.type.Number"
+	}
+	],
+	image: "evrythng/spot.png",
+	exec : function(state){
+		var propertyName = state.inputs.item(0).getValue();
+		
+		var endPointUrl = spotUrl + "/properties/" + propertyName + "?access_token=" + apiKeyGlobal;
+		this.setAsync();
+		var compo = this;
+		$.ajax({
+			beforeSend: function(xhrObj){
+				xhrObj.setRequestHeader("Accept","application/javascript");
+				xhrObj.setRequestHeader("Authorization", apiKeyGlobal);
+			},
+			url: endPointUrl,
+			type: "GET",
+			dataType: "json",
+			success: function(result){
+				// transform result into JSON
+				state.outputs.item(0).setValue(result[0].value);
+				compo.finishAsync();
+			},
+			error: function(msg){
+				compo.finishAsync();
+				console.log(evrythngMessage + "Error on: " + endPointUrl);
+				console.log(msg);
+			}
+		});
+
+	}
+});
+
